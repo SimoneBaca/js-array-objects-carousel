9146ed356bc6cf425a0bf49f40119275af6097e6
Simonebaca <simobaco99@gmail.com>
2023-03-23 21:49:30 +0100

    try add img+text

--- main.js
+++ main.js
@@ -12,26 +12,60 @@ Milestone 2:
 Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra.*/
 
 
-const images = [
+const data = [
     {
-        image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-        image: 'img/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
-        image: 'img/03.webp',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
-        image: 'img/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
-        image: 'img/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
-];
+      image: "img/01.webp",
+      title: "Marvel's Spiderman Miles Morales",
+      text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+    },
+    {
+      image: "img/02.webp",
+      title: "Ratchet & Clank: Rift Apart",
+      text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+    },
+    {
+      image: "img/03.webp",
+      title: "Fortnite",
+      text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    },
+    {
+      image: "img/04.webp",
+      title: "Stray",
+      text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
+    },
+    {
+      image: "img/05.webp",
+      title: "Marvel's Avengers",
+      text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+    },
+  ];
+// prendo gli elementi dal DOM
+// galleria
+const gallery = document.querySelector("#carousel .gallery");
+// bottoni
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+
+// creo ciclo per stampare le immagini e i testi
+let imageElements = "";
+data.forEach((game) => {
+  imageElements += `<img src="${game.image}" alt="${game.title}" />
+  <div class="caption text-white">
+    <h3>${game.title}</h3>
+    <p>${game.text}</p>
+  </div>`;
+});
+
+gallery.innerHTML = imageElements;
+
+// recupero le immagini
+const images = document.querySelectorAll(".gallery img");
+
+// recupero la caption
+const captions = document.querySelectorAll(".gallery .caption");
+
+// setto il primo oggetto come attivo
+
+let currentActiveIndex = 0;
+images[currentActiveIndex].classList.add("active");
+captions[currentActiveIndex].classList.add("active");
